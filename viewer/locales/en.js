d1d19ebe011bf6c006b60989e44b7baf2bdb00f1
Carlos Miguel Jenkins Pérez <carlos@jenkins.co.cr>
2014-09-18 03:26:34 -0600

    chg: dev: Setting English as default locale now that others locales are tested.

---
+++ viewer/locales/en.js
@@ -0,0 +1,5 @@
+/*
+ * English doesn't requires a jQuery UI datepicker localization file as
+ * it is the default. This is a dummy file that will be loaded empty to
+ * allow Frog Croak to continue it's workflow.
+ */
