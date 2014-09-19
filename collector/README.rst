504f651f45a3fbd22a03eb07d58c7ac7398cef5a
Carlos Jenkins <carlos@jenkins.co.cr>
2014-09-19 12:19:37 -0600

    new: dev: Preparing files for PyPI submission.

---
+++ collector/README.rst
@@ -0,0 +1,24 @@
+About
+=====
+
+Frog Croak is a tool that allows users to monitor and analyse their Internet
+connection speed over time.
+
+For more information please visit: https://github.com/carlos-jenkins/frog-croak/
+
+For a live demo visit: http://speed.jenkins.co.cr/
+
+.. image:: https://raw.githubusercontent.com/carlos-jenkins/frog-croak/master/screenshot.png
+        :target: http://speed.jenkins.co.cr/
+        :alt: Live Demo
+
+
+Frog Croak is composed of two separate programs:
+
+- *Viewer*: A Javascript application that allows to display and analyse
+  gathered data.
+- *Collector*: A Python application that collects speed tests data samples over
+  time.
+
+
+This Python package holds Frog Croak data Collector only, not the viewer.
--- collector/setup.py
+++ collector/setup.py
@@ -2,13 +2,38 @@
 
 from distutils.core import setup
 
+with open('README.rst') as fd:
+    long_description = fd.read()
+
+
 setup(
+    # Main
     name='frog-croak',
     version='1.0',
     package_dir={'' : 'lib'},
     packages=[
-        'frog-croak',
-        'frog-croak.speedtest'
+        'frog_croak',
+        'frog_croak.speedtest'
     ],
     scripts=['bin/frog-croak'],
+
+    # Extra metadata
+    author='Carlos Jenkins',
+    author_email='carlos@jenkins.co.cr',
+    url='https://github.com/carlos-jenkins/frog-croak/',
+    description='Internet connection speed and monitor tool.',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet',
+        'Topic :: System :: Monitoring',
+    ],
 )
